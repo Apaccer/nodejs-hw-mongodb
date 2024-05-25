31cd87304bfb1b758a80f2f1550d30888ce70377
Apaccer <142975619+Apaccer@users.noreply.github.com>
2024-05-25 13:40:33 +0300

    add_env-initMongo

---
+++ src/db/Contact.js
@@ -0,0 +1,10 @@
+import { Schema } from 'mongoose';
+
+// const contactSchema = new Schema({
+//     name: { type: String, required: true },
+// phoneNumber:  { type: String, required: true },
+// email: {type:}
+// isFavourite - boolean, default false
+// contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
+// createdAt - дата створення
+// updatedAt - дата оновлення});
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
-export const setupServer = () => {};
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  const PORT = Number(env('PORT', '3000'));
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+  });
+};
